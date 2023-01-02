f0568097c824e74e3b9cf5a13c1ee64db2339168
LCH6890 <lorenachamorroalameda@gmail.com>
2023-01-02 10:00:41 +0100

    se añade ejercicio 5

---
+++ Numeros.js
@@ -0,0 +1,16 @@
+//Altura en cm
+let altura = 164
+//Altura en metros
+let alturaM = 1.64
+//Peso en kg
+let peso = 80.2
+
+//Altura redondeada hacia arriba
+console.log(alturaM.toFixed())
+//Peso en kg redondeado hacia abajo
+console.log(peso.toPrecision(2))
+console.log(Math.round(peso))
+
+//Variable +1
+
+let max_valor_JS = Number.MAX_VALUE
